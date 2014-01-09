4085199aeef40252b014e123c5d332dc1d052b42
Oldřich Pulkrt <O.Pulkrt@gmail.com>
2014-01-09 15:57:41 +0100

    Added set of possible parameters of response from server

---
+++ src/communication/ResponseParam.java
@@ -0,0 +1,12 @@
+
+package communication;
+
+/**
+ * Set of possible response params
+ *
+ * @author Oldřich Pulkrt <O.Pulkrt@gmail.com>
+ */
+public enum ResponseParam
+{
+    SUCCESS, FAIL, CHECK, CHECKMATE, STALEMATE, CONNECTED, DISCONNECTED;
+}
