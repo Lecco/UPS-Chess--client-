79df3fb24f4b31e21b80bfbb2d8a5721f9047b2f
Oldřich Pulkrt <O.Pulkrt@gmail.com>
2014-01-09 15:55:23 +0100

    Added set of possible types of response from server

---
+++ src/communication/ResponseType.java
@@ -0,0 +1,13 @@
+
+package communication;
+
+/**
+ * Set of possible types of response from chess server
+ *
+ * @author Oldřich Pulkrt <O.Pulkrt@gmail.com>
+ * @version 1.0
+ */
+public enum ResponseType
+{
+    STATUS, MOVE, GAME_STATUS, WHITE_STATUS, BLACK_STATUS, MESSAGE;
+}
